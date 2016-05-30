4022d6a24dcb3cd9e76141d625270880c77b7f70
Elsis-Sitati <sitatielsis@gmail.com>
2016-05-30 14:17:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/elsis/mbukinyacoach/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.elsis.mbukinyacoach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
